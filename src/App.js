ebfa6f233876c5d03571d5197f6f6c22d01ca1a7
Kreshna Putra Adi Wicaksana <kreshnaputraadi31@student.uns.ac.id>
2022-07-26 14:33:16 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import UserForm from './components/UserForm';
+// import CounterOne from './components/CounterOne';
+// import CounterTwo from './components/CounterTwo';
+// import DocTitleOne from './components/DocTitleOne';
+// import DocTitleTwo from './components/DocTitleTwo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DocTitleOne/>
+      <DocTitleTwo/> */}
+      {/* <CounterOne/>
+      <CounterTwo/> */}
+
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
